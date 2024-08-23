56b4dd4a9bcb2870bf9fe9636159a4c2868f9156
Theo <170449321+illtrythisout@users.noreply.github.com>
2024-08-23 15:26:04 +0200

    Add functionality and style to slider

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-// Create the boxes
-    // Create a variable labeled nGridLines
-    // Make a reference to the sketchContainer div in the html
-    // Create a for loop that creates (nGridLines) squared amount of divs inside the sketchContainer
-
 let nGridLines = 16;
-
 const sketchContainer = document.querySelector("#sketchContainer");
-for (i = 0; i < (nGridLines * nGridLines); i++) {
+function createGrid() {
+    for (let i = 0; i < (nGridLines * nGridLines); i++) {
 
-    const pixel = document.createElement("div");
-    pixel.style["flex-basis"] = (100 / nGridLines) + "%";
-    pixel.style["height"] = (100 / nGridLines) + "%";
-    pixel.classList.add("pixel")
-    sketchContainer.appendChild(pixel);
+        const pixel = document.createElement("div");
+        pixel.style["flex-basis"] = (100 / nGridLines) + "%";
+        pixel.style["height"] = (100 / nGridLines) + "%";
+        pixel.classList.add("pixel")
+        sketchContainer.appendChild(pixel);
 
-    let pixelOpacity = 0.1;
-    pixel.addEventListener("mouseover", () => {
-        pixelOpacity = pixelOpacity + 0.1;
-        let pixelColor = "rgb(" + 128 + ", " + 0 + ", " + 0 + ", " + pixelOpacity + ")";
-        pixel.style["backgroundColor"] = pixelColor;
-    });
-}
+        let pixelOpacity = 0.1;
+        pixel.addEventListener("mouseover", () => {
+            pixelOpacity = pixelOpacity + 0.1;
+            let pixelColor = "rgba(" + 128 + ", " + 0 + ", " + 0 + ", " + pixelOpacity + ")";
+            pixel.style["backgroundColor"] = pixelColor;
+        })
+    }
+};
+function delGrid() {
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach((pixel) => {
+        sketchContainer.removeChild(pixel)
+    })
+};
+
+createGrid();
+
+const gridResSlider = document.querySelector("#gridRes");
+const gridResDisplay = document.querySelector("#gridResDisplay");
+gridResDisplay.textContent = gridResSlider.value;
+gridResSlider.oninput = function() {
+    gridResDisplay.textContent = gridResSlider.value;
+    delGrid();
+    nGridLines = gridResSlider.value;
+    createGrid();
+};
+
+
